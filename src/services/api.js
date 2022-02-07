719ecfb7a366b0460a7938a8a09979f4ba55df4d
blarth <jminocente@gmail.com>
2022-02-07 14:38:03 -0300

    chore: deply cercel with heroku link

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = " https://apimy-wallet.herokuapp.com/"
+const BASE_URL = " https://apimy-wallet.herokuapp.com"
 
 function createConfig(token) {
   return { headers: { Authorization: `Bearer ${token}` } };
